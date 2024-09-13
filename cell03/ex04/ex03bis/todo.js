a17d92f6a9a41c0ddfd02b8545c74fa0c0d2251a
Tintinkung <sukrit_dinosuar@hotmail.com>
2024-09-13 19:45:51 +0700

    feat: cell03 completion

---
+++ cell03/ex04/ex03bis/todo.js
@@ -0,0 +1,83 @@
+let list = [];
+const toDoEl = $("#ft_list");
+const addBtn = $("#addBtn");
+
+const render = () => {
+  toDoEl.empty();
+  if (!list.length) {
+    const empty_list = $("<div></div>")
+      .text("No ToDos")
+      .css({
+        color: "white",
+        "font-size": "20px",
+        "text-align": "center",
+      })
+      .addClass("emptyMessage");
+    toDoEl.append(empty_list);
+    return;
+  }
+
+  list.forEach((value, index) => {
+    const toDoItem = createTodoElement(value);
+    toDoItem.on("click", () => {
+      removeTodo(index);
+    });
+    toDoEl.append(toDoItem);
+  });
+};
+
+const createTodoElement = (value) => {
+  const button = $("<button></button>").addClass("todoItem").text(value);
+  return button;
+};
+
+const addTodo = (value) => {
+  list.push(value);
+  updateCookie(JSON.stringify(list));
+  render();
+};
+
+const removeTodo = (index) => {
+  list.splice(index, 1);
+  updateCookie(JSON.stringify(list));
+  render();
+};
+
+const updateCookie = (value) => {
+  setCookie("toDo", value, 7);
+};
+
+const setCookie = (key, value, days) => {
+  const date = new Date();
+  date.setTime(date.getTime() + days * 24 * 60 * 60 * 1000);
+  const expires = `expires=${date.toUTCString()}`;
+  document.cookie = `${key}=${value};${expires};path=/`;
+};
+
+const getCookie = (key) => {
+  const cookies = document.cookie.split(";");
+
+  for (let i = 0; i < cookies.length; i++) {
+    const cookie = cookies[i].trim();
+    if (cookie.startsWith(key + "=")) {
+      return cookie.substring(key.length + 1);
+    }
+  }
+  return null;
+};
+
+addBtn.on("click", () => {
+  const newTodo = prompt("New ToDo");
+  if (newTodo && newTodo.trim().length > 0) {
+    addTodo(newTodo);
+  }
+});
+
+const oldToDo = getCookie("toDo");
+if (oldToDo) {
+  list = JSON.parse(oldToDo);
+}
+
+$(document).ready(() => {
+  render();
+});
